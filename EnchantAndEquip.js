d3cde1c77ed7ab6cb26d8104209b4f1d38f32548
corbbing <cgappdev@gmail.com>
2013-12-02 13:37:28 -0800

    Create EnchantAndEquip.js

---
+++ EnchantAndEquip.js
@@ -0,0 +1,34 @@
+function item(){
+	this.address = null;
+	this.value = 0;
+	this.slot = "headSlot";
+	this.name = "Hood"
+}
+
+function player(){
+	this.stamina = 12;
+	this.health = 100;
+	this.damageLow = 1;
+	this.damageHigh = 3;
+	this.damageMult = 1;
+	this.eHealth = 0;
+	this.eStamina = 0;
+	this.eDamageMult = 0;
+	this.headSlot = null;
+	this.chestSlot = null;
+}
+
+player.prototype.getEquipValues=function(){
+	this.eHealth = 0;
+	this.eStamina = 0;
+	this.eDamageMult = 0;
+	if (this.headSlot != null){
+		this[this.headSlot.address] += this.headSlot.value;
+	}
+}
+
+player.prototype.equip = function(i){
+	this[i.slot] = i;
+}
+
+var p = new player();
